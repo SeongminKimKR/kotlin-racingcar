cb9e93bdeac9d7a3b9bd5abc98f0fd4d6c3dabba
SeongminKimKR <gvf156@gmail.com>
2024-11-14 00:00:32 +0900

    test: 클래스별 테스트 클래스 생성

---
+++ src/test/kotlin/step1/domain/InputParserTest.kt
@@ -0,0 +1,6 @@
+package step1.domain
+
+import io.kotest.core.spec.style.FunSpec
+
+class InputParserTest : FunSpec({
+})
---
+++ src/test/kotlin/step1/domain/OperandTest.kt
@@ -0,0 +1,5 @@
+package step1.domain
+import io.kotest.core.spec.style.FunSpec
+
+class OperandTest : FunSpec({
+})
