19909a2731408ad3d038e3e3b9c5d7b25e7ed221
SeongminKimKR <gvf156@gmail.com>
2024-11-14 00:55:03 +0900

    refactor: 연산 가능식 판별 변수 정의 * chore: 연산 가능 조건 추가

--- src/main/kotlin/step1/domain/ParsedExpression.kt
+++ src/main/kotlin/step1/domain/ParsedExpression.kt
@@ -7,13 +7,18 @@ class ParsedExpression(input: String?) {
 
     init {
         elements = InputParser.parse(input)
+        require(isCanCalculate())
     }
 
-    fun isCanCalculate() = elements.size >= 3
+    fun isCanCalculate() = elements.size >= MIN_SIZE && elements.size % 2 == 1
 
     fun getElement(): String = elements.poll()
 
     fun putElement(value: String) {
         elements.push(value)
+    }
+
+    companion object {
+        const val MIN_SIZE = 3
     }
 }
