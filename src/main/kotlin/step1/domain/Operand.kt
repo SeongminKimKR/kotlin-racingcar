c6c5521bd794d1b576ee5bffd0827cc27e1cea74
SeongminKimKR <gvf156@gmail.com>
2024-11-15 00:02:42 +0900

    refactor: 내부 프로퍼티 제거

--- src/main/kotlin/step1/domain/Operand.kt
+++ src/main/kotlin/step1/domain/Operand.kt
@@ -1,15 +1,12 @@
 package step1.domain
 
 class Operand(input: String?) {
-    private val _value: Double
-
     val value: Double
-        get() = _value
 
     init {
         requireNotNull(input) { "입력값은 null일 수 없습니다." }
         require(isNumeric(input)) { "입력값은 유효한 숫자여야 합니다." }
-        _value = input.toDouble()
+        value = input.toDouble()
     }
 
     private fun isNumeric(value: String): Boolean {
