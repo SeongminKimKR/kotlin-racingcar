de3aa7caecaf129f93b01768334e7e6e87c0a494
SeongminKimKR <gvf156@gmail.com>
2024-11-15 19:46:38 +0900

    refactor: 입력값 클래스 리팩토링

--- src/main/kotlin/racingcar/GameRunner.kt
+++ src/main/kotlin/racingcar/GameRunner.kt
@@ -3,14 +3,14 @@ package racingcar
 import racingcar.domain.Cars
 import racingcar.util.getRandom
 import racingcar.view.ResultView
-import racingcar.view.dto.InputDto
-import racingcar.view.dto.ResultDto
+import racingcar.view.attribute.InputViewAttribute
+import racingcar.view.attribute.ResultViewAttribute
 
 object GameRunner {
     private const val MIN_VALUE_TO_MOVE_CAR = 4
     private val RANDOM_VALUE_RANGE = 0..9
 
-    fun play(input: InputDto) {
+    fun play(input: InputViewAttribute) {
         val cars = Cars(input.carCount)
 
         println("실행 결과")
@@ -21,7 +21,7 @@ object GameRunner {
                     car.move()
                 }
             }
-            ResultView().resolve(ResultDto(cars))
+            ResultView().resolve(ResultViewAttribute(cars))
         }
     }
 
