4d1de231b825e8089e43401add3c431e82aeec4b
SeongminKimKR <gvf156@gmail.com>
2024-11-15 19:53:53 +0900

    test: 입력 값 테스트 작성

--- src/test/kotlin/racingcar/view/attribute/InputAttributeTest.kt
+++ src/test/kotlin/racingcar/view/attribute/InputAttributeTest.kt
@@ -1,6 +1,24 @@
 package racingcar.view.attribute
 
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FunSpec
+import java.lang.IllegalArgumentException
 
 class InputAttributeTest : FunSpec({
+
+    test("입력 값은 빈값이나 null이면 예외를 던진다.") {
+
+        shouldThrow<IllegalArgumentException> { InputViewAttribute("", null) }
+        shouldThrow<IllegalArgumentException> { InputViewAttribute("", "") }
+        shouldThrow<IllegalArgumentException> { InputViewAttribute(null, "") }
+        shouldThrow<IllegalArgumentException> { InputViewAttribute(null, null) }
+    }
+
+    test("입력 값은 양수가 아니면 예외를 던진다.") {
+
+        shouldThrow<IllegalArgumentException> { InputViewAttribute("-1", "0") }
+        shouldThrow<IllegalArgumentException> { InputViewAttribute("0", "-1") }
+        shouldThrow<IllegalArgumentException> { InputViewAttribute("-1", "-1") }
+        shouldThrow<IllegalArgumentException> { InputViewAttribute("0", "0") }
+    }
 })
