14dc0ed00ebb238c825e9c43de66408ecc0cc73d
SeongminKimKR <gvf156@gmail.com>
2024-11-16 21:20:30 +0900

    test: RacingGame 테스트 사항 추가

--- src/test/kotlin/racingcar/domain/RacingGameTest.kt
+++ src/test/kotlin/racingcar/domain/RacingGameTest.kt
@@ -1,7 +1,9 @@
 package racingcar.domain
 
 import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.ranges.shouldBeIn
 import io.kotest.matchers.shouldBe
+import racingcar.util.DefaultRandomGenerator
 import racingcar.util.FakeRandomGenerator
 
 class RacingGameTest : FunSpec({
@@ -10,5 +12,11 @@ class RacingGameTest : FunSpec({
         val racingGame = RacingGame(2, FakeRandomGenerator())
         racingGame.tryMove()
         racingGame.getLocations().forEach { it shouldBe 1 }
+    }
+
+    test("tryMove()는 난수 값이 최소 기준 값 이상이면 1칸 움직이고 그렇지 않으면 움직이지 않는다.") {
+        val racingGame = RacingGame(2, DefaultRandomGenerator())
+        racingGame.tryMove()
+        racingGame.getLocations().forEach { it shouldBeIn (0..1) }
     }
 })
